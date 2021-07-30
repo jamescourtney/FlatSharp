6e9ba11855475d44bbf5ef7a42afc46a452e8c7a
James Courtney <jcourtney@outlook.com>
2021-07-30 15:30:54 -0700

    Fix

--- src/Tests/FlatSharpTests/ClassLib/SharedStringClassLibTests.cs
+++ src/Tests/FlatSharpTests/ClassLib/SharedStringClassLibTests.cs
@@ -38,10 +38,10 @@ namespace FlatSharpTests
                 Assert.IsTrue(a == (SharedString)b);
                 Assert.IsTrue((SharedString)a == (SharedString)b);
                 Assert.AreEqual<SharedString>(a, b);
-                Assert.AreEqual(((SharedString)a).ToString(), b);
 
                 if (b is not null)
                 {
+                    Assert.AreEqual(((SharedString)b).ToString(), b);
                     Assert.IsTrue(((SharedString)b).Equals(a));
                 }
             }
