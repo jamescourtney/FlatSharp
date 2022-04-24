945c316cef9cb6c17a5b322dac664b42b592fad7
James Courtney <jcourtney@outlook.com>
2022-04-23 22:23:44 -0700

    Fix up tests

--- src/Tests/FlatSharpTests/ClassLib/InputBufferTests.cs
+++ src/Tests/FlatSharpTests/ClassLib/InputBufferTests.cs
@@ -421,9 +421,9 @@ public class InputBufferTests
             return ((IInputBuffer)innerBuffer).GetReadOnlyByteMemory(start, length);
         }
 
-        public TItem InvokeParse<TItem>(IGeneratedSerializer<TItem> serializer, int offset, int objectDepthLimit)
+        public TItem InvokeParse<TItem>(IGeneratedSerializer<TItem> serializer, in GeneratedSerializerParseArguments arguments)
         {
-            return ((IInputBuffer)innerBuffer).InvokeParse<TItem>(serializer, offset, objectDepthLimit);
+            return ((IInputBuffer)innerBuffer).InvokeParse<TItem>(serializer, arguments);
         }
 
         public byte ReadByte(int offset)
