2db67b5591628d80a2a51d0881a76da0d0dcf60f
James Courtney <jcourtney@outlook.com>
2022-11-01 01:36:23 -0700

    copyrights

--- src/FlatSharp.Runtime/Vectors/ImmutableList.cs
+++ src/FlatSharp.Runtime/Vectors/ImmutableList.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2021 James Courtney
+ * Copyright 2022 James Courtney
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-using System;
 
 namespace FlatSharp.Internal;
 
--- src/Tests/FlatSharpTests/ClassLib/ImmutableListTests.cs
+++ src/Tests/FlatSharpTests/ClassLib/ImmutableListTests.cs
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 James Courtney
+ * Copyright 2022 James Courtney
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
