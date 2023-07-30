bec5a1712a23928b38474326d8ee9b3590054692
James Courtney <jcourtney@outlook.com>
2023-07-30 00:17:38 -0700

    Add project reference not part of solution.

--- src/test/App/Program.cs
+++ src/test/App/Program.cs
@@ -1,7 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using FlatSharp;
+using FlatSharpEndToEndTests.FileIdentifiers;
 
 Console.WriteLine("Hello, World!");
 
 FlatSharpEndToEndTests.FileIdentifiers.HasId id = new() { Foo = 32 };
 FlatSharpEndToEndTests.FileIdentifiers.HasId.Serializer.Write(new byte[100], id);
+
+FlatSharpEndToEndTests.FileIdentifiers.NoId noId = new();
+
+NoId.Serializer.Write(new byte[100], new());
