bcecf41605bc45dd4016d72b252caae8a3aef4f6
James Courtney <jcourtney@outlook.com>
2023-07-29 21:17:24 -0700

    Indirect reference

---
+++ src/test/App/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FlatSharp;
+
+Console.WriteLine("Hello, World!");
+
+FlatSharpEndToEndTests.FileIdentifiers.HasId id = new() { Foo = 32 };
+FlatSharpEndToEndTests.FileIdentifiers.HasId.Serializer.Write(new byte[100], id);
