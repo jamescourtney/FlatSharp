8c8510b88f573b5a73975431982dab94ca4cf467
James Courtney <jcourtney@outlook.com>
2024-02-25 02:01:22 -0800

    Update packages and SpanWriter (#428)

--- src/FlatSharp.Runtime/IO/ISpanWriter.cs
+++ src/FlatSharp.Runtime/IO/ISpanWriter.cs
@@ -79,21 +79,6 @@ public interface ISpanWriter
     int GetStringBytes(Span<byte> destination, string value, Encoding encoding);
 
     /// <summary>
-    /// Invokes the <see cref="IGeneratedSerializer{T}.Write{TSpanWriter}(TSpanWriter, Span{byte}, T, SerializationContext)"/>
-    /// method using the type information for this serializer.
-    /// </summary>
-    /// <remarks>
-    /// IGeneratedSerializer is a generic interface that accepts a SpanWriter. However, with precise type information,
-    /// it is possible for SpanWriter instances to avoid vtable indirection when wrapped in a struct or passed as a struct. This
-    /// hook allows SpanWriter implementations to invoke the generated serializer in an efficient way.
-    /// </remarks>
-    void InvokeWrite<TItemType>(
-        IGeneratedSerializer<TItemType> serializer,
-        Span<byte> destination,
-        TItemType item,
-        SerializationContext context);
-
-    /// <summary>
     /// Invokes the <see cref="ISharedStringWriter.FlushWrites{TSpanWriter}(TSpanWriter, Span{byte}, SerializationContext)"/> method.
     /// </summary>
     void FlushSharedStrings(
