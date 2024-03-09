e79006ff68f886c14ba6c39405b76afa146a9fe6
James Courtney <jcourtney@outlook.com>
2024-03-09 02:46:41 -0800

    Update others

--- src/CreateCompilerTestFiles.ps1
+++ src/CreateCompilerTestFiles.ps1
@@ -3,12 +3,3 @@ dotnet build FlatSharp.Compiler/FlatSharp.Compiler.csproj -c Release -f net8.0
 # Native AOT stuff
 $fbs = (gci -r Tests/CompileTests/NativeAot/*.fbs) -join ";"
 dotnet FlatSharp.Compiler/bin/Release/net8.0/FlatSharp.Compiler.dll --nullable-warnings false --normalize-field-names true --input "$fbs" -o Tests/CompileTests/NativeAot
-
-# Stryker stuff
-$fbs = (gci -r Tests/Stryker/*.fbs) -join ";"
-dotnet FlatSharp.Compiler/bin/Release/net8.0/FlatSharp.Compiler.dll --nullable-warnings false --normalize-field-names true --input "$fbs" -o Tests/Stryker/CodeGen --mutation-testing-mode
-
-# CSharp8 Stuff
-$fbs = (gci -r Tests/FlatsharpEndToEndTests/*.fbs | where Name -ne "AccessModifiers.fbs") -join ";"
-dotnet FlatSharp.Compiler/bin/Release/net8.0/FlatSharp.Compiler.dll --nullable-warnings false --normalize-field-names true --input "$fbs" -o Tests/CompileTests/CSharp8
-
