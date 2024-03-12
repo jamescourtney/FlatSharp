617e7c1beca4695110f414edfa8a134be1e43fb1
James Courtney <jcourtney@outlook.com>
2024-03-12 02:47:24 -0700

    Remove extra sln

--- src/CreateCompilerTestFiles.ps1
+++
@@ -1,5 +0,0 @@
-dotnet build FlatSharp.Compiler/FlatSharp.Compiler.csproj -c Release -f net8.0
-
-# Native AOT stuff
-$fbs = (gci -r Tests/CompileTests/NativeAot/*.fbs) -join ";"
-dotnet FlatSharp.Compiler/bin/Release/net8.0/FlatSharp.Compiler.dll --nullable-warnings false --normalize-field-names true --input "$fbs" -o Tests/CompileTests/NativeAot
