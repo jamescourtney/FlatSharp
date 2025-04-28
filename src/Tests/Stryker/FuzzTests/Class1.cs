977a3baa656f7401bd536b301109d440fbb6d34f
James Courtney <jcourtney@outlook.com>
2025-04-28 11:41:31 -0700

    basic fuzz tests

---
+++ src/Tests/Stryker/FuzzTests/Class1.cs
@@ -0,0 +1,40 @@
+﻿namespace FuzzTests;
+
+using FlatSharp;
+using FlatSharpStrykerTests;
+using SharpFuzz;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args[0] == "GenerateData")
+        {
+            Root root = Root.CreateTestRoot<SimpleListFactory>();
+            byte[] temp = new byte[Root.Serializer.GetMaxSize(root)];
+            int bytesWritten = Root.Serializer.Write(temp, root);
+
+            Directory.CreateDirectory("TestCases");
+            File.WriteAllBytes("TestCases/TestCase.bin", temp.AsSpan()[..bytesWritten]);
+        }
+        else
+        {
+            FlatBufferDeserializationOption option = Enum.Parse<FlatBufferDeserializationOption>(args[0]);
+
+            Fuzzer.OutOfProcess.Run(stream =>
+            {
+                using MemoryStream ms = new();
+                stream.CopyTo(ms);
+                byte[] bytes = ms.ToArray();
+                var clone = new Root(Root.Serializer.Parse(bytes, option));
+            });
+        }
+    }
+
+    private class SimpleListFactory : ICreateCreateListFactory
+    {
+        public static CreateListCallback<T> GetCallback<T>() => items => new List<T>(items);
+    }
+}
